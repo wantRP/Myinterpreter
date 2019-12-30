39ee9bd8e61be6133a917c8afeabbf7bcfb128bb
zhaobean <duchuan1024@hotmail.com>
2019-12-30 10:15:02 +0800

    delimiter analyzes

--- lexer.h
+++ lexer.h
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 const std::set<std::string>delimiter{":=","=","+","-","*","/",";","<>"};
-const std::set<char>otherchar{',',' ','=','+','-','*','/','<','>'};
+const std::set<char>otherchar{',',' ','=','+','-','*','/','<','>',':','(',')'};
 bool legalchar(char c){ return isalpha(c)||otherchar.count(c)||isdigit(c); }
 bool legalinnumber(char c){ return isdigit(c)||c=='.'; }
 bool legalinid(char c){ return isalpha(c)||c=='_'||isdigit(c); }
+void classify(char* s){
+	
+}
 void parse(char* s){
 	for(int i=0;i<strlen(s);){
 		char c=s[i];
@@ -24,8 +27,21 @@ void parse(char* s){
 		} else if(!legalchar(c)){
 			printf("ILLEGAL CHARACTER: %c\n",c);
 			++i;
-		} else{
-		for()
+		} else{// symbols
+			std::string ts="";
+			if(s[i+1]){
+				ts=ts+s[i]+s[i+1];
+				if(delimiter.count(ts)){ //analyze token length=2
+					printf("%s\n",ts.c_str());
+					i=i+2;
+					continue;
+				}
+			}
+			char cc=s[i];
+			ts="";
+			ts=ts+cc;
+			printf("%s\n",ts.c_str());
+			i=i+1;
 		}
 	}
 
