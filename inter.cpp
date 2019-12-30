e21979925080fa2a34c398e359fadde417c6c194
天畅 赵 <duchuan1024@hotmail.com>
2019-12-30 16:51:04 +0800

    classify

--- inter.cpp
+++ inter.cpp
@@ -3,9 +3,12 @@
 int main(){
   char s[256];
   //gets(s);
-  parse("a:= 1+sdd+(1.34<> 192.168.1.1)aaa.ccc... ;");
+  printf(".2. a:= 1+sdd+(1.34<> 192.168.1.1 - 13..)aaa.ccc... ;end.");
+  parse((char*)".2. a:= 1+sdd+(1.34<> 192.168.1.1 - 13..)aaa.ccc... ;end");
   classify();
-  for(auto i:originalwords) std::cout<<"("<<i<<")";
+  
 	printf("\n-------\n");
   for(auto i:words) std::cout<<i<<std::endl;
+  printf("\n-------\n");
+  for(auto i:parsermessage) std::cout<<i<<std::endl;
 }
--- lexer.h
+++ lexer.h
@@ -1,35 +1,42 @@
 #include<bits/stdc++.h>
 #define pb push_back
 const std::set<std::string>delimiter{":=","=","+","-","*","/",";","<>"};
+const std::set<std::string>keyword{"begin","end","integer","bool","read","write","for","while","do","if","and","then","or","not","var","true","false","else"};
 const std::set<char>otherchar{',',' ','=','+','-','*','/','<','>',':','(',')',';'};
 std::vector<std::string>originalwords;
 std::vector<std::string>words;
+std::vector<std::string>parsermessage;
 bool legalchar(char c){ return isalpha(c)||otherchar.count(c)||isdigit(c); }
 bool legalinnumber(char c){ return isdigit(c)||c=='.'; }
 bool legalinid(char c){ return isalpha(c)||c=='_'||isdigit(c); }
-bool classify(){
+int classify(){
 	for(auto s:originalwords){
-		if(s==" ") continue;
-		if(legalinnumber(s[0])){//number
-			bool dot=0;
+		if(s==" ") continue;//ignore space
+		if(legalinnumber(s[0])){//number,auto cut the second dot
+			bool point=0;
 			bool number=0;
 			bool cut=0;
 			for(int i=0;i<s.length();++i){
-				if(s[i]=='.'&&dot){
+				if(s[i]=='.'&&point){
+					parsermessage.push_back("waring: too many points, converted \'"+s+"'\ to \'"+s.substr(0,i)+"\'.");
 					s=s.substr(0,i);
 					break;
 				}
-				if(s[i]=='.') dot=1;
+				if(s[i]=='.') point=1;
 			}
 			if(s==".") {
+				parsermessage.push_back("error: unexcepted point");
 				//printf("%s:num expected but . found",s.c_str());
-				words.pb("err");
-				return 1;
+				//words.pb("err");
+				continue;
 			}
 			if(s[0]=='.') s="0"+s;
 			if(s[s.length()-1]=='.') s=s+"0";
-
+			
 		}//end anlz num
+		else if(legalinid(s[0])){
+			if(keyword.count(s)) s=s+", keyword";else s=s+",user id";
+		}
 		words.pb(s);
 	}//end for
 	return 0;
